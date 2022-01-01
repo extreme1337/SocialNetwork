648405f127415c27d51d20804ff170201906b98c
Marko Miseljic <marko.miseljic.14@gmail.com>
2022-01-01 12:45:31 +0100

    refactoring and added view and page for followers

--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, Dislike, UserSearch
+from .views import PostListView, ListFollowers, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, Dislike, UserSearch
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -7,11 +7,12 @@ urlpatterns = [
     path('post/edit/<int:pk>', PostEditView.as_view(), name='post-edit'),
     path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', Dislike.as_view(), name='dislike'),
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
-    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
-    path('post/<int:pk>/dislike', Dislike.as_view(), name='dislike'),
+    path('profile/<int:pk>/followers/', ListFollowers.as_view(), name="list-followers"),
     path('search/', UserSearch.as_view(), name='profile-search'),
 ]
