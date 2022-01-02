9f980f0e6d46c3cc4270af3a6181cbbf2ce01de3
Marko Miseljic <marko.miseljic.14@gmail.com>
2022-01-02 11:00:31 +0100

    c

--- src/settings.py
+++ src/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
